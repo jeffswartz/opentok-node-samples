6b64a80be608614957eb5d45efccd2b208604088
Jeff Swartz <swartz@tokbox.com>
2015-10-23 12:52:30 -0700

    Crimes agin humanity mock up

--- public/js/helloworld.js
+++ public/js/helloworld.js
@@ -1,14 +1,37 @@
 var session = TB.initSession(sessionId);
 var publisher = TB.initPublisher(apiKey, 'publisher');
+var subs = [];
+var cardsDiv = document.getElementById('cards');
 
 session.on({
   sessionConnected: function(event) {
-    session.publish(publisher);
+    session.publish(publisher, function(error) {
+      var card = document.createElement('img');
+      card.setAttribute('src', '/img/cah-question.png');
+      card.setAttribute('width', '264');
+      cards.appendChild(card);
+    });
   },
   streamCreated: function(event) {
     var subOptions = {insertMode: 'append'};
-    session.subscribe(event.stream, 'subscribers', subOptions);
+    var sub = session.subscribe(event.stream, 'subscribers', subOptions, function(error) {
+      subs.push(sub);
+      arrangeSubs();
+      var card = document.createElement('img');
+      console.log('/img/cah-answer' + (subs.length - 1) + '.png')
+      card.setAttribute('src', '/img/cah-answer' + (subs.length - 1) + '.png');
+      //card.setAttribute('src', '/img/cah-answer0.png');
+      card.setAttribute('width', '264');
+      cards.appendChild(card);
+    });
   }
 });
+
+function arrangeSubs() {
+  for (var i = 0; i < subs.length; i++) {
+    sub = subs[i];
+    sub.element.style.left = i * 264 + 'px';
+  }
+}
 
 session.connect(apiKey, token);
