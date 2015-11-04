27427099b105ce04d2e94064006f1877be89c1dd
Jeff Swartz <swartz@tokbox.com>
2015-11-04 15:46:35 -0800

    Archiving sample

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     OpenTok = require('opentok');
 
-var apiKey = process.env.API_KEY,
-    apiSecret = process.env.API_SECRET;
+var apiKey = process.env.API_KEY;
+var apiSecret = process.env.API_SECRET;
+var sessionId;
 
 if (!apiKey || !apiSecret) {
   console.log('You must specify API_KEY and API_SECRET environment variables');
@@ -11,23 +13,65 @@ if (!apiKey || !apiSecret) {
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var opentok = new OpenTok(apiKey, apiSecret);
 
-opentok.createSession(function(err, session) {
+opentok.createSession({ mediaMode: 'routed' }, function(err, session) {
   if (err) throw err;
   app.set('sessionId', session.sessionId);
   init();
 });
 
 app.get('/', function(req, res) {
-  var sessionId = app.get('sessionId'),
+  sessionId =  app.get('sessionId'),
       token = opentok.generateToken(sessionId);
 
   res.render('index.ejs', {
     apiKey: apiKey,
     sessionId: sessionId,
     token: token
+  });
+});
+
+app.get('/download/:archiveId', function(req, res) {
+  var archiveId = req.param('archiveId');
+  opentok.getArchive(archiveId, function(err, archive) {
+    if (err) return res.send(500, 'Could not get archive '+archiveId+'. error='+err.message);
+    res.redirect(archive.url);
+  });
+});
+
+app.post('/start', function(req, res) {
+  /*
+  var hasAudio = (req.param('hasAudio') !== undefined);
+  var hasVideo = (req.param('hasVideo') !== undefined);
+  var outputMode = req.param('outputMode');
+  console.log('apiKey', apiKey);
+  */
+  opentok.startArchive(app.get('sessionId'), {
+    name: 'Node Archiving Sample App'
+  }, function(err, archive) {
+    if (err) {
+      console.log('err ', err.message);
+      return res.send(500,
+        'Could not start archive for session ' + sessionId + '. Error: ' + err.message
+      );
+    }
+    res.json(archive);
+  });
+});
+
+app.get('/stop/:archiveId', function(req, res) {
+  var archiveId = req.param('archiveId');
+  opentok.stopArchive(archiveId, function(err, archive) {
+    if (err) {
+      return res.send(500, 'Could not stop archive ' + archiveId + '. error='
+        + err.message);
+      }
+    res.json(archive);
   });
 });
 
--- public/js/helloworld.js
+++ public/js/helloworld.js
@@ -1,14 +1,49 @@
+var archiveId;
 var session = TB.initSession(sessionId);
 var publisher = TB.initPublisher(apiKey, 'publisher');
 
 session.on({
   sessionConnected: function(event) {
     session.publish(publisher);
+    $('#startArchive').show();
   },
   streamCreated: function(event) {
     var subOptions = {insertMode: 'append'};
     session.subscribe(event.stream, 'subscribers', subOptions);
+  },
+  archiveStarted: function(event) {
+    archiveId = event.id;
   }
 });
 
 session.connect(apiKey, token);
+
+$('#startArchive').click(function() {
+  $.ajax({
+    type: 'POST',
+    url: '/start',
+    success: function(responseData, textStatus, jqXHR) {
+      console.log('Archive started.');
+      var obj = responseData;
+    },
+    error: function (responseData, textStatus, errorThrown) {
+      console.log('startArchive POST failed.', responseData, textStatus, errorThrown);
+    }
+  });
+  $('#startArchive').hide();
+  $('#stopArchive').show();
+});
+
+$('#stopArchive').click(function() {
+  $.ajax({
+    type: 'GET',
+    url: '/stop/' + archiveId,
+    success: function(responseData, textStatus, jqXHR) {
+      console.log('Archive stopped.');
+    },
+    error: function (responseData, textStatus, errorThrown) {
+      console.log('startArchive POST failed.', responseData, textStatus, errorThrown);
+    }
+  });
+  $('#stopArchive').hide();
+});
